491f3786f540f503aeb43034abce38333948895f
Russsstle <rjohn.sants@yahoo.com>
2021-08-10 09:58:43 +0800

    Fixed syntax error

--- config/mail.php
+++ config/mail.php
@@ -71,13 +71,13 @@ return [
             'transport' => 'array',
         ],
     ],
-    'stream' => [
+   'stream' => [
         'ssl' => [
             'allow_self_signed' => true,
             'verify_peer' => false,
             'verify_peer_name' => false,
         ],
-    ]
+    ],
     /*
     |--------------------------------------------------------------------------
     | Global "From" Address
